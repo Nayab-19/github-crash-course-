ef03bce13981b2d5c969bd5337f55a0305f6da42
Nayab-19 <nayabshaikh1906@gmail.com>
2024-03-10 20:43:23 +0530

    replace concat with template literal

--- index.js
+++ index.js
@@ -3,11 +3,6 @@ const name = 'Nayab Shaikh'
 
 
 function sayName(){
-    console.log('My Name is Nayab Shaikh')
-<<<<<<< HEAD
+    console.log('My Name is ${name}') 
 }
 
-
-=======
-}
->>>>>>> parent of 8f49ec5 (Added isAbove fn)
